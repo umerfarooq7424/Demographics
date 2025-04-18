856351b92fb8451137c15f2c069818a0f1cc1595
umerfarooq7424 <umergojra@gmail.com>
2025-04-18 09:03:56 -0400

    Docs Updated

--- main.py
+++ main.py
@@ -333,7 +333,7 @@ try:
         
     file_path = "Demographic Multipliers Overview.docx"
     with open(file_path, "rb") as file:
-        st.download_button("Download Document", file, file_name="Demographic Multipliers Overview.docx")
+        st.download_button("Download Document", file, file_name="Demographic Multipliers Overview. Quick Guide. Glossary.docx")
     # st.markdown(f'[Click to open in Word](./{file_path})', unsafe_allow_html=True)
 
     with st.expander("Glossary and User Guide"):
